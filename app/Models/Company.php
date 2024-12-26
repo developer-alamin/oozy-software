03c4e1d3e6820dbc1c1f724c905ea9b1bdc1ad27
Alamin <121603191+developer-alamin@users.noreply.github.com>
2024-12-26 09:44:07 +0600

    Database Migration Update

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $table = "companies";
+    protected $fillable = [
+        'uuid',
+        'name',
+        'type',
+        'description',
+        'status',
+        'meta_data',
+        'creator_id',
+        'creator_type',
+        'updater_id',
+        'updater_type',
+    ];
+   
 }
